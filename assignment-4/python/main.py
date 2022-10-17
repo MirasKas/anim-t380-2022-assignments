60ff7b70e13e34dc5c4a48b2a88afc776c1fdde2
MirasKas <mdk89@drexel.edu>
2022-10-17 17:08:38 -0400

    Assignment 4 Update
    changed cmds to mc

--- assignment-4/python/main.py
+++ assignment-4/python/main.py
@@ -1,24 +1,24 @@
 import os
 import sys
-import maya.cmds as cmds
+import maya.cmds as mc
 import maya.standalone
 
 maya.standalone.initialize()
 
 O_Name = 'goldfish.model.mdk'
 
-if cmds.file(ex=(fileName)):
-    cmds.file(o=(fileName))
+if mc.file(ex=(fileName)):
+    mc.file(o=(fileName))
 
 else:
-    cmds.file (new=True)
-    cmds.file(rn=(os.path.join(O_Name + '.1')))
+    mc.file (new=True)
+    mc.file(rn=(os.path.join(O_Name + '.1')))
 
 
 
-newName = cmds.file(sn=True).split('.')
+newName = mc.file(sn=True).split('.')
 newName[3] = str(int(newName[3])+1)
 
-cmds.file(rn=((os.path.join(os.getcwd(),newName.join('.')))))
+mc.file(rn=((os.path.join(os.getcwd(),newName.join('.')))))
 
-cmds.file(save=True, typ=('mayaAscii'))
+mc.file(save=True, typ=('mayaAscii'))
